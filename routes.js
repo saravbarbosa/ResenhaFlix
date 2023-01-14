ea0138be2955645ee8b74e1d69d2283467ac6cc5
matias <matias21.oliveirape@gmail.com>
2023-01-13 21:37:27 -0300

    busca completa e edição no back

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8081;
-const {Op} = require("sequelize")
+const { Op, where } = require("sequelize")
 const bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({extended:false})
+var urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -16,219 +16,263 @@ const Sequelize = require('sequelize')
 const sequelizeInstance = new Sequelize('resenhaflix', 'root', 'root', {
 	host: 'localhost',
 	dialect: 'mysql'
-  })
-  sequelizeInstance.authenticate().then(function () {
+})
+sequelizeInstance.authenticate().then(function () {
 	console.log("Conectado!!")
-  }).catch(function (erro) {
+}).catch(function (erro) {
 	console.log("Erro ao conectar: " + erro)
-  })
+})
 
-  const resenha = sequelizeInstance.define('resenha', {
+const resenha = sequelizeInstance.define('resenha', {
 	codigo: {
-	  type: Sequelize.INTEGER,
-	  autoIncrement: true,
-	  primaryKey: true
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
 	},
-	filme:{
+	filme: {
 		type: Sequelize.STRING
 	},
 	resenha: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	genero: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	diretor: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	ano: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	}
-  });
-  resenha.sync()
+});
+resenha.sync({ alter: true })
 
-  const usuario = sequelizeInstance.define('usuario', {
+const usuario = sequelizeInstance.define('usuario', {
 	codigo: {
-	  type: Sequelize.INTEGER,
-	  autoIncrement: true,
-	  primaryKey: true
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		primaryKey: true
 	},
 	nome: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	email: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	senha: {
-	  type: Sequelize.STRING
+		type: Sequelize.STRING
 	},
 	dataNasc: {
-	  type: Sequelize.DATE
+		type: Sequelize.DATE
 	},
 	preferencias: {
 		type: Sequelize.STRING
 	},
-	
-  });
 
-  usuario.sync()
-  
+});
 
+usuario.sync({ alter: true })
 
-app.use(express.static(__dirname +'/public'))
+app.use(express.static(__dirname + '/public'))
 
-app.get("/" , (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/" + "public/inicio.html")
 })
 
-app.get("/resenhaDom" , (req, res) => {
+app.get("/resenhaDom", (req, res) => {
 	res.sendFile(__dirname + "/" + "public/resenha.html")
 })
 
 app.get("/cadResenha", (req, res) => {
-    res.sendFile(__dirname + "/"+ "public/adicionar.html")
+	res.sendFile(__dirname + "/" + "public/adicionar.html")
 })
 
 app.post("/Resenha", urlEncodedParser, (req, res) => {
-    var filme = req.body.filme
-    var nresenha = req.body.resenha
-    var genero = req.body.genero
+	var id = req.body.id
+	var filme = req.body.filme
+	var nresenha = req.body.resenha
+	var genero = req.body.genero
 	var diretor = req.body.diretor
-    var ano = req.body.ano
-   // var id = 0
-
-
-    //var resenhas = {id: id,filme: filme, resenha: nresenha, genero: genero, diretor: diretor, ano: ano}
-
+	var ano = req.body.ano
+	const rese = {
+		filme: filme, resenha: nresenha, genero: genero, diretor: diretor, ano: ano
+	}
+	if (id != null){
+		const resen = resenha.update(rese,{
+			where: {
+				id: id
+			}
+		})
+	} else {
 	const resenhass = resenha.build({
 		filme: filme, resenha: nresenha, genero: genero, diretor: diretor, ano: ano
 	})
+	
 	resenhass.save()
-
-   res.redirect("/");
+   }
+	res.redirect("/");
 })
 
-app.get('/buscaResenha',(req,res)=>{
-    res.sendFile(__dirname +"/"+"/public/buscar.html")
+app.get("/cadUsuario", (req, res) => {
+	res.sendFile(__dirname + "/" + "public/adicionarUsuario.html")
+})
+
+app.post("/Usuario", urlEncodedParser, (req, res) => {
+	var id = req.body.id
+
+	var nomes = req.body.nome
+	var emails = req.body.email
+	var dataN = req.body.data
+	var preferenciass = req.body.preferencias
+	const user = { nome: nomes, email: emails, dataNasc: dataN, preferencias: preferenciass }
+
+	if (id != null) {
+		const usuarioss = usuario.update(user, {
+			where: {
+			 id: id 
+			}
+		}
+		)
+	} else {
+	const usuarioss = usuario.build(user)
+	resenhass.save()
+}
+
+res.redirect("/");
+})
+
+
+app.get('/buscaResenha', (req, res) => {
+	res.sendFile(__dirname + "/" + "/public/buscar.html")
 });
 
-app.get('/Resenha', async (req,res) => {
-    var filmes = req.query.filme
-    var generos = req.query.genero
-    var anos = req.query.ano
-
-    //filmes = filmes.replaceAll(" ","")
-	//generos = generos.replaceAll(" ", "")
-	//anos = anos.replaceAll(" ", "")
+app.get('/Resenha', async (req, res) => {
+	var filmes = req.query.filme
+	var generos = req.query.genero
+	var anos = req.query.ano
 	let ub = ""
-		
-		if(filmes.length > 0 && generos == "" && anos == ""){
-			
-			const encontrado = resenha.findAll({
-				where:{
-					[Op.substring]: [
-						{filme: filmes},
-					],
+
+	if (filmes.length > 0 && generos == "" && anos == "") {
+
+		const encontrado = await resenha.findAll({
+			where: {
+				filme: {
+					[Op.substring]: filmes
 				}
-			})
-			ub = encontrado
+			}
+		})
+		ub = encontrado
 
-		};
-		if (filmes.length > 0 && generos.length > 0 && anos == "") {
-			const encontrado = resenha.findAll({
-				where:{
-					filme: {
-						[Op.substring]: Sequelize.literal(filmes)
-					},
-					genero: {
-						[Op.substring]: Sequelize.literal(generos)
-					}
+	};
+	if (filmes.length > 0 && generos.length > 0 && anos == "") {
+		const encontrado = await resenha.findAll({
+			where: {
+				filme: {
+					[Op.substring]: filmes
+				},
+				genero: {
+					[Op.substring]: generos
 				}
-			})
-			ub = encontrado
-		}
-		if (filmes.length > 0 && generos.length > 0 && anos.length > 0) {
-			const encontrado = resenha.findAll({
-				where:{
-					[Op.substring]: [
-						{filme: filmes},
-						{genero: generos},
-						{ano: anos}
-					]
+			}
+		})
+		ub = encontrado
+	}
+	if (filmes.length > 0 && generos.length > 0 && anos.length > 0) {
+		const encontrado = await resenha.findAll({
+			where: {
+				filme: {
+					[Op.substring]: filmes
+				},
+				genero: {
+					[Op.substring]: generos
+				},
+				ano: {
+					[Op.substring]: anos
 				}
-			})
-			ub = encontrado
 
-		}
-		if (filmes == "" && generos.length > 0 && anos.length > 0) {
-			const encontrado = resenha.findAll({
-				where:{
-					[Op.substring]: [
-						{ano: anos},
-						{genero: generos}
-					]
+			}
+		})
+		ub = encontrado
+
+	}
+	if (filmes == "" && generos.length > 0 && anos.length > 0) {
+		const encontrado = await resenha.findAll({
+			where: {
+
+				genero: {
+					[Op.substring]: generos
+				},
+				ano: {
+					[Op.substring]: anos
 				}
-			})
-			ub = encontrado
 
-		}
-		if (filmes.length > 0 && generos == ""  && anos.length > 0) {
-			const encontrado = resenha.findAll({
-				where:{
-					[Op.substring]: [
-						{filme: filmes},
-						{ano: anos}
-					]
+			}
+		})
+		ub = encontrado
+
+	}
+	if (filmes.length > 0 && generos == "" && anos.length > 0) {
+		const encontrado = await resenha.findAll({
+			where: {
+				filme: {
+					[Op.substring]: filmes
+				},
+
+				ano: {
+					[Op.substring]: anos
 				}
-			})
-			ub = encontrado
 
-		}
-		
-		if (filmes == "" && generos.length > 0 && anos == "") {
-			const encontrado = await resenha.findAll({
-				
-				where:{
-					genero: generos
+			}
+		})
+		ub = encontrado
+
+	}
+
+	if (filmes == "" && generos.length > 0 && anos == "") {
+		const encontrado = await resenha.findAll({
+
+			where: {
+				genero: generos
+			}
+		})
+		ub = encontrado
+
+	}
+
+	if (filmes == '' && generos == '' && anos.length > 0) {
+		const encontrado = await resenha.findAll({
+			where: {
+				ano: {
+					[Op.substring]: anos
 				}
-			})
-			ub = encontrado
 
-		}
-		
-		if (filmes == '' && generos == '' && anos.length > 0) {
-			 resenha.findAll({
-				where:{
-					[Op.substring]: [
-						{ano: anos},
-					]
-				}
-			})
-			ub = encontrado
-		}
+			}
+		})
+		ub = encontrado
+	}
 
-		var exibicao = ""
-		var film = []
-		ub = JSON.stringify(ub)
-		ub = JSON.parse(ub)
-		console.log(ub)
-		
-		for(var i=0; i < ub.length;i++){
-			
-			exibicao+= "Titulo: "+ub[i].filme + " ";
-			exibicao+= "Resenha: "+ub[i].resenha + " ";
-			exibicao+= "Genero: "+ub[i].genero + " ";
-			exibicao+= "Diretor: "+ub[i].diretor + " ";
-			exibicao+= "Ano: "+ub[i].ano +  ""
+	var exibicao = ""
+	var film = []
+	ub = JSON.stringify(ub)
+	ub = JSON.parse(ub)
+	console.log(ub)
 
-			film.push(exibicao)
-			exibicao = ""
+	for (var i = 0; i < ub.length; i++) {
 
-			
-		 }
-		
-		res.render(__dirname + "/public/busca-realizada.html",  {filme :film});
-	
+		exibicao += "Titulo: " + ub[i].filme + " ";
+		exibicao += "Resenha: " + ub[i].resenha + " ";
+		exibicao += "Genero: " + ub[i].genero + " ";
+		exibicao += "Diretor: " + ub[i].diretor + " ";
+		exibicao += "Ano: " + ub[i].ano + ""
+
+		film.push(exibicao)
+		exibicao = ""
+
+
+	}
+
+	res.render(__dirname + "/public/busca-realizada.html", { filme: film });
+
 })
 
 
